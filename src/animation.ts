de0e52752c6c15e91cccfd6485f346846da92e6f
Aradu92 <alradulescu@ucsd.edu>
2025-10-09 14:50:16 -0700

    added animation

---
+++ src/animation.ts
@@ -0,0 +1,4 @@
+export function animateFeature(featureName: string): void {
+  console.log(`\x1b[1mParty! Party! Party! - ${featureName}\x1b[0m`);
+  console.log(`\x1b[3mEnjoy your ${featureName.toLowerCase()}!\x1b[0m`);
+}
